13184d810e64143490082254d952b72786c140a0
mosismoki <mosirezaei@outlook.com>
2025-08-04 19:36:54 +0400

    Initialize Firebase and anonymous auth on startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'firebase_options.dart';
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  final userCredential = await FirebaseAuth.instance.signInAnonymously();
-  // Print the UID of the signed-in user to the console.
-  // ignore: avoid_print
-  print('Signed in anonymously as ${userCredential.user?.uid}');
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  await FirebaseAuth.instance.signInAnonymously();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
